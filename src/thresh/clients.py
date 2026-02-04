989a7bf273d417570748bb167277e45042b5e4c3
joe <joseph.todd2001@googlemail.com>
2026-02-04 17:20:10 +0000

    added aiohttp ssl closure fix

--- src/thresh/clients.py
+++ src/thresh/clients.py
@@ -3,8 +3,7 @@ import asyncio
 
 from contextlib import asynccontextmanager
 
-from typing import Final, AsyncIterator, AsyncContextManager
-
+from typing import Final, AsyncIterator
 
 
 class RiotAPIClient():
@@ -22,39 +21,12 @@ class RiotAPIClient():
         try:
             yield cls(session) 
         finally:
+            from thresh.helpers import create_aiohttp_closed_event
 
-            # Handle https://github.com/aio-libs/aiohttp/issues/1925#issuecomment-592596034
-            # 
-            transports = 0
-            all_is_lost = asyncio.Event()
-            if len(session.connector._conns) == 0:
-                all_is_lost.set()
-            for conn in session.connector._conns.values():
-                for handler, _ in conn:
-                    proto : asyncio.Protocol = getattr(handler.transport, "_ssl_protocol", None)
-                    if proto is None:
-                        continue
+            all_is_lost: asyncio.Event = create_aiohttp_closed_event(session)
+            await all_is_lost.wait()
 
-                    transports += 1
-                    orig_lost = proto.connection_lost
-                    orig_eof_recieved = proto.eof_received
-
-                    def connection_lost(exc):
-                        orig_lost(exc)
-                        nonlocal transports
-                        transports -= 1
-                        if transports == 0:
-                            all_is_lost.set()
-
-                    def eof_recieved():
-                        try:
-                            orig_eof_recieved()
-                        
-                        # handle case were eof_recieved is called after 
-                        # _app_protocol and _transport are set to None.
-                        except AttributeError:
-                            pass
-
+                    
 
     async def get_from_test_url(self):
         await asyncio.sleep(0)
@@ -63,15 +35,18 @@ class RiotAPIClient():
 
 if __name__ == "__main__":
 
+    # BUG: causes ubclosed client session
     async def main():
         async with RiotAPIClient.connect() as client:
-            await client.get_from_test_url()
+            # await client.get_from_test_url()
+            ...
 
     async def main2():
 
         async with aiohttp.ClientSession() as session:
             client = RiotAPIClient(session=session)
-            await client.get_from_test_url()
+            # await client.get_from_test_url()
+            ...
 
 
-    asyncio.run(main())
+    asyncio.run(main2())
---
+++ src/thresh/helpers.py
@@ -0,0 +1,53 @@
+import asyncio
+import functools
+
+
+def create_aiohttp_closed_event(session) -> asyncio.Event:
+    """Work around aiohttp issuethat doesn't properly close transports on exit.
+
+    See https://github.com/aio-libs/aiohttp/issues/1925#issuecomment-592596034
+
+    Returns:
+       An event that will be set once all transports have been properly closed.
+    """
+
+    transports = 0
+    all_is_lost = asyncio.Event()
+
+    if len(session.connector._conns) == 0:
+        all_is_lost.set()
+        return all_is_lost
+
+    def connection_lost(exc, orig_lost):
+        nonlocal transports
+
+        try:
+            orig_lost(exc)
+        finally:
+            transports -= 1
+            if transports == 0:
+                all_is_lost.set()
+
+    def eof_received(orig_eof_received):
+        try:
+            orig_eof_received()
+        except AttributeError:
+            # It may happen that eof_received() is called after
+            # _app_protocol and _transport are set to None.
+            pass
+
+    for conn in session.connector._conns.values():
+        for handler, _ in conn:
+            proto = getattr(handler.transport, "_ssl_protocol", None)
+            if proto is None:
+                continue
+
+            transports += 1
+            orig_lost = proto.connection_lost
+            orig_eof_received = proto.eof_received
+
+            proto.connection_lost = functools.partial(connection_lost, orig_lost=orig_lost)
+            proto.eof_received = functools.partial(eof_received, orig_eof_received=orig_eof_received)
+
+    return all_is_lost
+
