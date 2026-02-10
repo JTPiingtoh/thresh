d50abc0c0ac36319352b1cc58b3805dddc8b3422
joe <joseph.todd2001@googlemail.com>
2026-02-10 19:15:36 +0000

    updated set limiter state

--- tests/test.py
+++ tests/test.py
@@ -1,16 +1,15 @@
-limits = "100:120,20:1"
-counts = "1:120,1:1"
+limits = "100:120,20:1,99:120,9:12"
+counts = "1:120,1:1,2:120,3:12"
 
 limits = limits.split(",")
 counts = counts.split(",")
 
 limits_counts = list(zip(limits, counts))
 
-def get_rates(_sorted_limits_count):
-    requests, per_second = _sorted_limits_count[0].split(":")
+def get_rate(_limit_count):
+    requests, per_second = _limit_count[0].split(":")
     return float(requests) / float(per_second)
 
+sorted_limits_counts = sorted(limits_counts, key = get_rate)
 
-sorted_limits_counts = sorted(limits_counts, key = get_rates)
-
-print(sorted_limits_counts)
+print(sorted_limits_counts[0])
